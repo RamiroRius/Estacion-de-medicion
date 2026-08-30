4355d67c4ce820bc30e7f08660ae2ea63e626d10
RamiroRius <ramirorius7@gmail.com>
2026-08-29 23:53:01 -0300

    base de datos con sqlite3 agregada, ademas de la insercion de los datos en el programa de python

--- web/app.py
+++ web/app.py
@@ -2,11 +2,32 @@ import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 import serial
+import sqlite3
 
 app = Flask(__name__)
 
 PUERTO_SERIAL = os.environ.get("PUERTO_SERIAL", "/dev/ttyACM0")
 BAUD_RATE = 9600
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "..", "data")
+DB_PATH = os.path.join(DATA_DIR, "mediciones.db")
+
+
+conexion = sqlite3.connect(DB_PATH)
+cursor = conexion.cursor()
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS datos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        lectura INTEGER NOT NULL,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+    """
+)
+conexion.commit()
+conexion.close()
+
 
 try:
     ser = serial.Serial(PUERTO_SERIAL, BAUD_RATE, timeout=1)
@@ -15,11 +36,18 @@ except serial.SerialException as e:
     ser = None
 
 socketio = SocketIO(app, async_mode='threading', cors_allowed_origins="*")
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
+def insertDataBase(valor):
+    conexion = sqlite3.connect(DB_PATH)
+    cursor = conexion.cursor()
+    cursor.execute(
+        "INSERT INTO datos (lectura) VALUES (?)", (valor,)
+    )
+    conexion.commit()
+    conexion.close()
 
 def mostrar_datos():
     while True:
@@ -31,6 +59,7 @@ def mostrar_datos():
             if linea:
                 print(f"Enviando dato: {linea}")
                 socketio.emit("actualizar_sensor", {"valor": linea})
+                insertDataBase(linea)
         except serial.SerialException as e:
             print(f"Error de conexión serial: {e}")
             socketio.emit("error_sensor", {"mensaje": "Sensor desconectado"})
